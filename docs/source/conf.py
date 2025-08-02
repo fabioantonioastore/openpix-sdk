7aa03473bebf2ea6cc1d561fd464d1bf2d846bc4
ninja <astore.a.fabio@gmail.com>
2025-08-01 21:46:41 -0300

    version 0.1.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,25 +11,22 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'OpenPix SDK'
-copyright = '2025, Fabio Antonio Astore'
-author = 'Fabio Antonio Astore'
-release = '0.1.0'
+project = "OpenPix SDK"
+copyright = "2025, Fabio Antonio Astore"
+author = "Fabio Antonio Astore"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
-exclude_patterns = [
-    "sphinx.ext.autodoc"
-]
-
+templates_path = ["_templates"]
+exclude_patterns = ["sphinx.ext.autodoc"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
